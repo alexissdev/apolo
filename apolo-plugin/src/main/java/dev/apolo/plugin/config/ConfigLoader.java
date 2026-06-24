5aec0feec91c0824b608203f724aca76e23c34ac
alexissdev <notcacha1998@gmail.com>
2026-06-24 10:20:59 -0300

    merge missing config keys into existing config.yml on startup
    Use copyDefaults(true) + saveConfig() so new config keys added in future
    versions are automatically written to existing installations without
    requiring a manual file reset.

--- apolo-plugin/src/main/java/dev/apolo/plugin/config/ConfigLoader.java
+++ apolo-plugin/src/main/java/dev/apolo/plugin/config/ConfigLoader.java
@@ -11,6 +11,8 @@ public class ConfigLoader {
 
     public ApoloConfig load() {
         plugin.saveDefaultConfig();
+        plugin.getConfig().options().copyDefaults(true);
+        plugin.saveConfig();
         ApoloConfig config = new ApoloConfig();
         config.load(plugin);
         return config;
